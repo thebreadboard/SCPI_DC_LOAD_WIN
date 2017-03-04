ca361cab7289a4071a57aae218e8fbda3622859f
Peter Oakes <peter@thebreadboard.ca>
2017-03-03 19:58:53 -0500

    First create of project

---
+++ DC Load UI/DC Load UI/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace DC_Load_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dcLoadPort = new System.IO.Ports.SerialPort(this.components);
+            this.RefreshPorts = new System.Windows.Forms.Button();
+            this.txtConsole = new System.Windows.Forms.RichTextBox();
+            this.comPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sdfsadf = new System.Windows.Forms.Label();
+            this.deviceID = new System.Windows.Forms.TextBox();
+            this.refreshID = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDACValue = new System.Windows.Forms.TextBox();
+            this.setDAC = new System.Windows.Forms.Button();
+            this.readADC = new System.Windows.Forms.Button();
+            this.txtADCResponse = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RefreshPorts
+            // 
+            this.RefreshPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RefreshPorts.Location = new System.Drawing.Point(684, 12);
+            this.RefreshPorts.Name = "RefreshPorts";
+            this.RefreshPorts.Size = new System.Drawing.Size(106, 33);
+            this.RefreshPorts.TabIndex = 1;
+            this.RefreshPorts.Text = "Refresh";
+            this.RefreshPorts.UseVisualStyleBackColor = true;
+            this.RefreshPorts.Click += new System.EventHandler(this.RefreshPorts_Click);
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsole.Location = new System.Drawing.Point(463, 170);
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.Size = new System.Drawing.Size(327, 232);
+            this.txtConsole.TabIndex = 2;
+            this.txtConsole.Text = "";
+            // 
+            // comPorts
+            // 
+            this.comPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comPorts.FormattingEnabled = true;
+            this.comPorts.Location = new System.Drawing.Point(80, 12);
+            this.comPorts.Name = "comPorts";
+            this.comPorts.Size = new System.Drawing.Size(598, 33);
+            this.comPorts.TabIndex = 3;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ports";
+            // 
+            // sdfsadf
+            // 
+            this.sdfsadf.AutoSize = true;
+            this.sdfsadf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sdfsadf.Location = new System.Drawing.Point(12, 59);
+            this.sdfsadf.Name = "sdfsadf";
+            this.sdfsadf.Size = new System.Drawing.Size(47, 25);
+            this.sdfsadf.TabIndex = 6;
+            this.sdfsadf.Text = "IDN";
+            // 
+            // deviceID
+            // 
+            this.deviceID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deviceID.Location = new System.Drawing.Point(80, 59);
+            this.deviceID.Name = "deviceID";
+            this.deviceID.Size = new System.Drawing.Size(598, 31);
+            this.deviceID.TabIndex = 7;
+            // 
+            // refreshID
+            // 
+            this.refreshID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshID.Location = new System.Drawing.Point(684, 59);
+            this.refreshID.Name = "refreshID";
+            this.refreshID.Size = new System.Drawing.Size(106, 33);
+            this.refreshID.TabIndex = 8;
+            this.refreshID.Text = "Refresh";
+            this.refreshID.UseVisualStyleBackColor = true;
+            this.refreshID.Click += new System.EventHandler(this.refreshID_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ADCs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "DAC1";
+            // 
+            // txtDACValue
+            // 
+            this.txtDACValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDACValue.Location = new System.Drawing.Point(80, 170);
+            this.txtDACValue.Name = "txtDACValue";
+            this.txtDACValue.Size = new System.Drawing.Size(224, 31);
+            this.txtDACValue.TabIndex = 11;
+            // 
+            // setDAC
+            // 
+            this.setDAC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.setDAC.Location = new System.Drawing.Point(310, 168);
+            this.setDAC.Name = "setDAC";
+            this.setDAC.Size = new System.Drawing.Size(106, 33);
+            this.setDAC.TabIndex = 12;
+            this.setDAC.Text = "SET";
+            this.setDAC.UseVisualStyleBackColor = true;
+            this.setDAC.Click += new System.EventHandler(this.setDAC_Click);
+            // 
+            // readADC
+            // 
+            this.readADC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readADC.Location = new System.Drawing.Point(310, 218);
+            this.readADC.Name = "readADC";
+            this.readADC.Size = new System.Drawing.Size(106, 33);
+            this.readADC.TabIndex = 13;
+            this.readADC.Text = "Refresh";
+            this.readADC.UseVisualStyleBackColor = true;
+            this.readADC.Click += new System.EventHandler(this.readADC_Click);
+            // 
+            // txtADCResponse
+            // 
+            this.txtADCResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtADCResponse.Location = new System.Drawing.Point(80, 218);
+            this.txtADCResponse.Name = "txtADCResponse";
+            this.txtADCResponse.Size = new System.Drawing.Size(224, 156);
+            this.txtADCResponse.TabIndex = 14;
+            this.txtADCResponse.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(458, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Console Messages";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(802, 414);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtADCResponse);
+            this.Controls.Add(this.readADC);
+            this.Controls.Add(this.setDAC);
+            this.Controls.Add(this.txtDACValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.refreshID);
+            this.Controls.Add(this.deviceID);
+            this.Controls.Add(this.sdfsadf);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comPorts);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.RefreshPorts);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "Form1";
+            this.Text = "DC Load UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort dcLoadPort;
+        private System.Windows.Forms.Button RefreshPorts;
+        private System.Windows.Forms.RichTextBox txtConsole;
+        private System.Windows.Forms.ComboBox comPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label sdfsadf;
+        private System.Windows.Forms.TextBox deviceID;
+        private System.Windows.Forms.Button refreshID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDACValue;
+        private System.Windows.Forms.Button setDAC;
+        private System.Windows.Forms.Button readADC;
+        private System.Windows.Forms.RichTextBox txtADCResponse;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DC Load UI/DC Load UI/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace DC_Load_UI
+{
+
+    public partial class Form1 : Form
+    {
+        String txtIDNQuery = "*IDN?";
+        String txtDACWrite = "DEVE:DAC1";
+        String txtADC1Query = "DEVE:ADC1?";
+        String txtADC2Query = "DEVE:ADC2?";
+        String txtADC3Query = "DEVE:ADC3?";
+        String txtADC4Query = "DEVE:ADC4?";
+        DeviceManager myComPorts = new DeviceManager();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            myComPorts.PropertyChanged += new  PropertyChangedEventHandler(comPorts_ListChanged);
+
+        }
+        List<String> tList = new List<String>();
+
+        private void RefreshPorts_Click(object sender, EventArgs e)
+        {
+            RefreshPortList();
+        }
+        private void RefreshPortList()
+        {
+            comPorts.SelectedIndexChanged -= comPorts_SelectedIndexChanged;
+            comPorts.DataSource = new BindingSource(myComPorts.SerialPorts, null);
+            comPorts.DisplayMember = "Key";
+            comPorts.ValueMember = "Value";
+            comPorts.Invoke(comPorts => comPorts.Refresh());
+            comPorts.SelectedIndexChanged += new EventHandler(comPorts_SelectedIndexChanged);
+
+        }
+        private void readADC_Click(object sender, EventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)comPorts.SelectedValue;
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+
+            txtADCResponse.Clear();
+            try
+            {
+                serialPort.Open();
+                // Send an ID Query
+                serialPort.WriteLine(txtADC1Query);
+                string message = Read(serialPort);
+                if (message == string.Empty) { txtADCResponse.AppendText(txtADC1Query + " Error" + Environment.NewLine); }
+                else { txtADCResponse.AppendText(txtADC1Query + " = " + message + Environment.NewLine); }
+                serialPort.WriteLine(txtADC2Query);
+                message = Read(serialPort);
+                if (message == string.Empty) { txtADCResponse.AppendText(txtADC2Query + " Error" + Environment.NewLine); }
+                else { txtADCResponse.AppendText(txtADC2Query + " = " + message + Environment.NewLine); }
+                serialPort.WriteLine(txtADC3Query);
+                message = Read(serialPort);
+                if (message == string.Empty) { txtADCResponse.AppendText(txtADC3Query + " Error" + Environment.NewLine); }
+                else { txtADCResponse.AppendText(txtADC3Query + " = " + message + Environment.NewLine); }
+                serialPort.WriteLine(txtADC4Query);
+                message = Read(serialPort);
+                if (message == string.Empty) { txtADCResponse.AppendText(txtADC4Query + " Error" + Environment.NewLine); }
+                else { txtADCResponse.AppendText(txtADC4Query + " = " + message + Environment.NewLine); }
+
+            }
+            catch (Exception ex)
+            {
+                txtConsole.Clear();
+                txtConsole.AppendText(ex.Message);
+            }
+            finally
+            {
+                serialPort.Close();
+            }
+
+        }
+
+    private void refreshID_Click(object sender, EventArgs e)
+        {
+            refreshIDs();
+        }
+        private void refreshIDs()
+        {
+            txtConsole.Clear();
+            SerialPort serialPort = (SerialPort)comPorts.SelectedValue;
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+            try
+            {
+                serialPort.Open();
+                // Send an ID Query
+                serialPort.WriteLine(txtIDNQuery);
+                string message = Read(serialPort);
+                deviceID.Text = message;
+            }
+            catch (Exception ex)
+            {
+                txtConsole.Clear();
+                txtConsole.AppendText(txtIDNQuery + " :- " + ex.Message);
+            }
+            finally
+            {
+                serialPort.Close();
+            }
+        }
+
+
+    public string Read(SerialPort serial)
+    {
+       string message = string.Empty;
+
+            try
+            {
+                message = serial.ReadLine();
+                
+            }
+            catch (TimeoutException ex) {
+                    txtConsole.Clear();
+                    txtConsole.AppendText(ex.Message);
+                }
+            return message;
+    }
+    private void setDAC_Click(object sender, EventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)comPorts.SelectedValue;
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+            int valDAC;
+            bool isNumeric = int.TryParse(txtDACValue.Text, out valDAC);
+            txtConsole.Clear();
+            if (isNumeric)
+            {
+
+                try
+                {
+                    serialPort.Open();
+                    // Send an DAC Write
+                    string sendmessage = txtDACWrite + " #H" + valDAC.ToString("X4"); ;
+                    serialPort.WriteLine(sendmessage);
+                    //string message = Read(serialPort);
+                    //if (message == "Error") { txtConsole.AppendText(txtDACWrite + " Error" + Environment.NewLine); }
+                    //else { txtConsole.AppendText(txtDACWrite + " = " + message + Environment.NewLine); }
+                }
+                catch (Exception ex)
+                {
+                    txtConsole.Clear();
+                    txtConsole.AppendText(ex.Message);
+                }
+                finally
+                {
+                    serialPort.Close();
+                }
+            }
+            else {
+                txtConsole.Clear();
+                txtConsole.AppendText("please provide an int between 0 and 65535");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshPortList();
+            //do this here so it does not trigger before the drop down is ready
+            comPorts.SelectedIndexChanged += new EventHandler(comPorts_SelectedIndexChanged);
+        }
+
+
+        private void comPorts_ListChanged(object sender, EventArgs e)
+        {
+            RefreshPortList();
+        }
+
+        private void comPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            refreshIDs();
+        }
+    }
+    /// <summary>
+    ///     Provides automated detection and initiation of serial devices.
+    /// </summary>
+    public sealed class DeviceManager : IDisposable, INotifyPropertyChanged
+    {
+        /// <summary>
+        ///     A System Watcher to hook events from the WMI tree.
+        /// </summary>
+        private readonly ManagementEventWatcher _deviceWatcher = new ManagementEventWatcher(new WqlEventQuery(
+            "SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2 OR EventType = 3"));
+
+        /// <summary>
+        ///     A list of all dynamically found SerialPorts.
+        /// </summary>
+        private Dictionary<string, SerialPort> _serialPorts = new Dictionary<string, SerialPort>();
+
+        /// <summary>
+        ///     Initialises a new instance of the <see cref="DeviceManager"/> class.
+        /// </summary>
+        public DeviceManager()
+        {
+            // Attach an event listener to the device watcher.
+            _deviceWatcher.EventArrived += _deviceWatcher_EventArrived;
+
+            // Start monitoring the WMI tree for changes in SerialPort devices.
+            _deviceWatcher.Start();
+
+            // Initially populate the devices list.
+            DiscoverDevices();
+        }
+
+        /// <summary>
+        ///     Gets a list of all dynamically found SerialPorts.
+        /// </summary>
+        /// <value>A list of all dynamically found SerialPorts.</value>
+        public Dictionary<string, SerialPort> SerialPorts
+        {
+            get { return _serialPorts; }
+            private set
+            {
+                _serialPorts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Stop the WMI monitors when this instance is disposed.
+            _deviceWatcher.Stop();
+        }
+
+        /// <summary>
+        ///     Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Handles the EventArrived event of the _deviceWatcher control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArrivedEventArgs"/> instance containing the event data.</param>
+        private void _deviceWatcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            DiscoverDevices();
+        }
+
+        /// <summary>
+        ///     Dynamically populates the SerialPorts property with relevant devices discovered from the WMI Win32_SerialPorts class.
+        /// </summary>
+        private void DiscoverDevices()
+        {
+            // Create a temporary dictionary to superimpose onto the SerialPorts property.
+            var dict = new Dictionary<string, SerialPort>();
+
+            try
+            {
+                // Scan through each SerialPort registered in the WMI.
+                foreach (ManagementObject device in
+                    new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_SerialPort").Get())
+                {
+                    // Create a SerialPort to add to the collection.
+                    var port = new SerialPort();
+
+                    // Gather related configuration details for the Arduino Device.
+                    var config = device.GetRelated("Win32_SerialPortConfiguration")
+                                       .Cast<ManagementObject>().ToList().FirstOrDefault();
+
+                    // Set the SerialPort's PortName property.
+                    port.PortName = device["DeviceID"].ToString();
+
+                    // Set the SerialPort's BaudRate property. Use the devices maximum BaudRate as a fallback.
+                    port.BaudRate = (config != null)
+                                        ? int.Parse(config["BaudRate"].ToString())
+                                        : int.Parse(device["MaxBaudRate"].ToString());
+
+                    // Add the SerialPort to the dictionary.
+                    dict.Add(device["Name"].ToString(), port);
+                }
+
+                // Return the dictionary.
+                SerialPorts = dict;
+            }
+            catch (ManagementException mex)
+            {
+                // Send a message to debug.
+                Debug.WriteLine(@"An error occurred while querying for WMI data: " + mex.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Called when a property is set.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        //[NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public static class Extensions
+    {
+        public static void Invoke<TControlType>(this TControlType control, Action<TControlType> del)
+            where TControlType : Control
+        {
+            if (control.InvokeRequired)
+                control.Invoke(new Action(() => del(control)));
+            else
+                del(control);
+        }
+    }
+}
